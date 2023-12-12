ece0c2b22553429212d5cd7b6f3ce53ca4c6e316
lenne sebastien <sebastienlenne@gmail.com>
2023-12-12 09:49:50 +0100

    add file task 0-Start a script for projet markdown2html

---
+++ markdown2html.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+"""Module to convert markdown to html"""
+
+# Task 0- Start a script
+# This script takes two arguments: first is a markdown file name and the
+# second is an html output filename
+
+if __name__ == '__main__':
+    import sys
+    import os
+    if len(sys.argv) < 3:
+        print('Usage: ./markdown2html.py README.md README.html',
+              file=sys.stderr)
+        sys.exit(1)
+
+    if not os.path.isfile(sys.argv[1]):
+        print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
+        sys.exit(1)
+
+sys.exit(0)
